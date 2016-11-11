c017e9b02020e53aaaab6dbdd8315fabf9f83ea4
Paul Enriquez <pkaenriquez@gmail.com>
2016-11-11 15:00:23 +0800

    Worked on Outboxes controller and Outbox model

--- app/controllers/outboxes_controller.rb
+++ app/controllers/outboxes_controller.rb
@@ -2,12 +2,17 @@ class OutboxesController < ApplicationController
     def index
         @outboxes = Outbox.all
     end
+
+    # new: Generate New Outbox Message Form
     def new
         @outbox = Outbox.new
     end
+
+    # create: Save new Outbox Message, and Send
     def create
         @outbox = Outbox.new(outbox_params)
-        if @outbox.save
+        @outbox.message_type = "SEND"
+        if @outbox.Save
             
         else
         end
@@ -16,6 +21,13 @@ class OutboxesController < ApplicationController
         @outbox = Outbox.find(params[:id])
         send_message(@outbox)
     end
+
+    # edit: Generate Re-send Outbox Message Form
+    def edit
+        @outbox = Outbox.find_by(params[:id])
+    end
+
+    
     def destroy
 
     end
@@ -23,8 +35,7 @@ class OutboxesController < ApplicationController
     private def outbox_params
         params.require(:outbox).permit!
     end
-    private def send_message()
-        chikka_post_request_url = "https://post.chikka.com/smsapi/request"
+    private def send_message
         HTTParty.post(chikka_post_request_url, :body => message.to_json)
     end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module Mis21SmsApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded."
+    config.chikka_api_post_request_url = "https://post.chikka.com/smsapi/request"
+    config.chikka_api_shortcode = "29290000007"
+    config.chikka_api_client_id = "2f12212f84113ccafbae7d78630d15fe35f50a84107d5b04037bfd04afb6471a"
+    config.chikka_api_secret_key = "0b965f20ab2bbb7dcadd50fb91843cb8d9632a512ff249a3a1e45a4d6eb32f32"
   end
 end
