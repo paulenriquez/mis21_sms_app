c0cf5a85ffd4de8ff8825e3631d37425bd90446f
Paul Enriquez <pkaenriquez@gmail.com>
2016-11-14 02:36:42 +0800

    Completed all DB Functions of the Outbox controller

--- app/controllers/outboxes_controller.rb
+++ app/controllers/outboxes_controller.rb
@@ -8,28 +8,36 @@ class OutboxesController < ApplicationController
         @outbox = Outbox.new
     end
 
-    # create: Save new Outbox Message, and Send
+    # create: Save new Outbox Message
     def create
         @outbox = Outbox.new(outbox_params)
         @outbox.message_type = "SEND"
-        if @outbox.Save
-            
+        if @outbox.save
+            redirect_to(outbox_path(@outbox.id))
         else
+            redirect_to(new_outbox_path)
         end
     end
+
+    # show: Show created Outbox Message, then send
     def show
         @outbox = Outbox.find(params[:id])
-        send_message(@outbox)
     end
 
     # edit: Generate Re-send Outbox Message Form
     def edit
-        @outbox = Outbox.find_by(params[:id])
+        @outbox = Outbox.find(params[:id])
     end
-
     
-    def destroy
-
+    # update: Save edited Outbox Message as new Message
+    def update
+        @outbox = Outbox.new(outbox_params)
+        @outbox.message_type = "SEND"
+        if @outbox.save
+            redirect_to(outbox_path(@outbox.id))
+        else
+            redirect_to(new_outbox_path)
+        end
     end
 
     private def outbox_params
--- app/models/outbox.rb
+++ app/models/outbox.rb
@@ -1,15 +1,15 @@
 class Outbox < ApplicationRecord
-    before_save: complete_outbox_attributes
+    before_save :complete_outbox_attributes
 
     private def complete_outbox_attributes
         self.shortcode = Rails.application.config.chikka_api_shortcode
         self.message_id = generate_message_id
-        self.client_id = Rails.application.config.chikka_api_config
+        self.client_id = Rails.application.config.chikka_api_client_id
         self.secret_key = Rails.application.config.chikka_api_secret_key
     end
     private def generate_message_id
         message_id = SecureRandom.hex
-        while Outbox.exists(message_id: message_id) do
+        while Outbox.exists?(message_id: message_id) do
             message_id = SecureRandom.hex
         end
         message_id
