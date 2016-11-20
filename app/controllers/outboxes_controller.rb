8203e00857600b21fb68347dddda51f8486e4a86
Paul Enriquez <pkaenriquez@gmail.com>
2016-11-21 02:02:22 +0800

    Worked on Inbox receive function

--- app/controllers/outboxes_controller.rb
+++ app/controllers/outboxes_controller.rb
@@ -3,13 +3,12 @@ class OutboxesController < ApplicationController
         @outboxes = Outbox.all
     end
 
-    # new: Generate New Outbox Message Form
     def new
         @outbox = Outbox.new
-        @outbox.message = params[:message]
+        @outbox.mobile_number = params[:mobile_number] if not params[:mobile_number].blank?
+        @outbox.message = params[:message] if not params[:message].blank?
     end
 
-    # create: Save new Outbox Message
     def create
         @outbox = Outbox.new(outbox_params)
         if @outbox.save
@@ -20,25 +19,8 @@ class OutboxesController < ApplicationController
         end
     end
 
-    # show: Show created Outbox Message, then send
     def show
         @outbox = Outbox.find(params[:id])
-    end
-
-    # edit: Generate Re-send Outbox Message Form
-    def edit
-        @outbox = Outbox.find(params[:id])
-    end
-    
-    # update: Save edited Outbox Message as new Message
-    def update
-        @outbox = Outbox.new(outbox_params)
-        if @outbox.save
-            send_message(@outbox)
-            redirect_to(outbox_path(@outbox.id))
-        else
-            redirect_to(new_outbox_path)
-        end
     end
 
     private def outbox_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
+
+  post 'inbox/receive', to: 'inboxes#create'
   
   resources :outboxes, path: 'outbox'
-  resources :inboxes, path: 'inbox'
+  resources :inboxes, path: 'inbox', except: :new, :create
 end
